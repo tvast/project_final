29418a66afe07cc41f2c44ee209e9853378b5a9a
theophile vast <tvast@voyages-sncf.com>
2016-09-20 16:29:44 +0200

    add rest api to angular 2

---
+++ todo-app/todo-api/app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/todos');
+
+var app = express();
+
+// load mongoose package
+var mongoose = require('mongoose');
+
+// Use native Node promises
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/todo-api')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('Client IP:', ip);
+  next();
+});
+
+app.use('/todos/:id', function (req, res, next) {
+  console.log('Request Type:', req.method);
+  next();
+});
+
+app.get('/todos/:id', function (req, res, next) {
+  Todo.findById(req.params.id, function(err, todo){
+    if(err) res.send(err);
+    res.json(todo);
+  });
+});
+
+app.use('/', routes);
+var todos = require('./routes/todos');
+app.use('/todos', todos);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
